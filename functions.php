e13c1d601351ebc006b0730695f29065c0d810d5
Jonathan Roley <jonathan.roley@gmail.com>
2024-02-28 15:25:45 -0500

    update version number

--- functions.php
+++ functions.php
@@ -6,9 +6,9 @@
  * @since  1.0.8
  *
  *
- * @return ChurchPlugins_1_0_21
+ * @return ChurchPlugins_1_0_22
  * @author Tanner Moushey, 4/13/23
  */
 function churchplugins() {
-	return ChurchPlugins_1_0_21::initiate();
+	return ChurchPlugins_1_0_22::initiate();
 }
